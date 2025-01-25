4b8247fa1b6e522ce27f2d5dbec3445d8747dcd6
Chongshan0Lin <chongshan0lin@gmail.com>
2025-01-24 22:14:29 -0500

    Change GPU index to 2

--- GCN/victim.py
+++ GCN/victim.py
@@ -38,8 +38,8 @@ class victim:
             print("CUDA is not available. PyTorch will use the CPU.")
         # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        device = torch.device(f"cuda:{gpu_index}"if torch.cuda.is_available() else "cpu")
-        self.model.to(device)
+        # device = torch.device(f"cuda:{gpu_index}"if torch.cuda.is_available() else "cpu")
+        # self.model.to(device)
         print("parameters:",self.model.parameters())
 
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01, weight_decay=5e-4)
