9025d1374c821ecf3bb137bc7f3c38c47d59cb32
Chongshan0Lin <chongshan0lin@gmail.com>
2025-01-24 21:53:28 -0500

    GPU

--- GCN/layers.py
+++ GCN/layers.py
@@ -2,21 +2,21 @@ import torch
 import torch.nn as nn
 import numpy as np
 gpu_index = 2
+device = torch.device(f"cuda:{gpu_index}"if torch.cuda.is_available() else "cpu")
 
 class GCNLayer(nn.Module):
     def __init__(self, in_features, out_features, bias = True):
         # print("in feature:", in_features)
         # print("out feature:", out_features)
         super(GCNLayer, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
-        device = torch.device(f"cuda:{gpu_index}"if torch.cuda.is_available() else "cpu")
-        self.weight.to(device)
+        self.in_features = in_features.to(device)
+        self.out_features = out_features.to(device)
+        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features)).to(device)
+        # self.weight.to(device)
         print("weight device:", device)
         
         if bias:
-            self.bias = nn.Parameter(torch.FloatTensor(out_features))
+            self.bias = nn.Parameter(torch.FloatTensor(out_features)).to(device)
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
@@ -29,19 +29,20 @@ class GCNLayer(nn.Module):
 
     def forward(self, x, adj_norm):
 
-        adj_norm = torch.from_numpy(adj_norm).to(torch.float32)
+        adj_norm = torch.from_numpy(adj_norm).to(torch.float32).to(device)
 
         if isinstance(x, torch.Tensor) :
             x = x.detach().numpy()
         xnp = np.vstack(x).astype(np.float32)
-        x = torch.from_numpy(xnp)
-        device = torch.device(f"cuda:{gpu_index}"if torch.cuda.is_available() else "cpu")
-        x.to(device)
+        # device = torch.device(f"cuda:{gpu_index}"if torch.cuda.is_available() else "cpu")
+
+        x = torch.from_numpy(xnp).to(device)
+        # x.to(device)
 
         print("x device:", device)
-        support = torch.matmul(x, self.weight)
+        support = torch.matmul(x, self.weight).to(device)
 
-        out = torch.matmul(adj_norm, support)
+        out = torch.matmul(adj_norm, support).to(device)
 
         if self.bias is not None:
             out = out + self.bias
