bdc79217d48497e1f9a1e620ae337f8d3a629e86
Chongshan0Lin <chongshan0lin@gmail.com>
2025-01-24 22:16:09 -0500

    Change GPU index to 2

--- GCN/model.py
+++ GCN/model.py
@@ -15,8 +15,8 @@ class GCN(nn.Module):
         self.gc2 = GCNLayer(in_features=hidden_features, out_features=out_features)
         self.dropout = dropout
         device = torch.device(f"cuda:{gpu_index}"if torch.cuda.is_available() else "cpu")
-        self.gc1.to(device)
-        self.gc2.to(device)
+        # self.gc1.to(device)
+        # self.gc2.to(device)
         # self.to(device)
     
     def forward(self, x, adj_norm):
