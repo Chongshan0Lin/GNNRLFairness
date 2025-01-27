2708e5a62b129a20587e062f7b0f791b02ddd75f
Chongshan0Lin <chongshan0lin@gmail.com>
2025-01-27 02:40:13 -0500

    database

--- GCN/victim.py
+++ GCN/victim.py
@@ -122,13 +122,20 @@ class victim:
         return surrogate, DP, EOd, CDP
 
     def fair_metric(pred, labels, sens):
-        idx_s0 = sens==0
-        idx_s1 = sens==1
-        idx_s0_y1 = np.bitwise_and(idx_s0, labels==1)
-        idx_s1_y1 = np.bitwise_and(idx_s1, labels==1)
-        parity = abs(sum(pred[idx_s0])/sum(idx_s0)-sum(pred[idx_s1])/sum(idx_s1))
-        equality = abs(sum(pred[idx_s0_y1])/sum(idx_s0_y1)-sum(pred[idx_s1_y1])/sum(idx_s1_y1))
-        return parity.item(), equality.item()
+        pred = pred.cpu().numpy()
+        labels = labels.cpu().numpy()
+        sens = sens.cpu().numpy()
+        
+        idx_s0 = sens == 0
+        idx_s1 = sens == 1
+        idx_s0_y1 = np.bitwise_and(idx_s0, labels == 1)
+        idx_s1_y1 = np.bitwise_and(idx_s1, labels == 1)
+        
+        epsilon = 1e-8
+        parity = abs(pred[idx_s0].sum() / (idx_s0.sum() + epsilon) - pred[idx_s1].sum() / (idx_s1.sum() + epsilon))
+        equality = abs(pred[idx_s0_y1].sum() / (idx_s0_y1.sum() + epsilon) - pred[idx_s1_y1].sum() / (idx_s1_y1.sum() + epsilon))
+        
+        return parity, equality
 
 
     def change_edge(self, node1, node2):
