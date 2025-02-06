3785f3432766f9d60e5d5c5b54ceb5cfc2b15a4d
Chongshan0Lin <chongshan0lin@gmail.com>
2025-02-06 02:50:56 -0500

    added required datasets

--- GCN/data_loading.py
+++ GCN/data_loading.py
@@ -191,7 +191,7 @@ def load_pokec(dataset, label_number=1000):  # 1000
     idx_test = torch.LongTensor(idx_test)
     labels = torch.LongTensor(labels)
     features=torch.cat([features,sens.unsqueeze(-1)],-1)
-    adj = mx_to_torch_sparse_tensor(adj,return_tensor_sparse=return_tensor_sparse)
+    adj = mx_to_torch_sparse_tensor(adj,return_tensor_sparse=True)
 
     return adj, features, labels, idx_train, idx_val, idx_test, sens
 
